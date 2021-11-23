42308e5fd3d0451299501fb0cfba62a4062ec65d
Константин Песчанов <constantine.peschanov@gmail.com>
2021-11-23 22:17:48 +0200

    added graceful shutdown

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const http = require('http');
-const { port } = require('./config');
+const {port} = require('./config');
 const requestHandler = require('./src/server/request-handler');
 
-http
-  .createServer(requestHandler)
-  .listen(port, () => {
+function enableGracefulShutdown() {
+  const exitHandler = error => {
+    if (error) console.error(error);
+
+    console.log('Init Graceful shutdown...');
+    process.exit();
+  };
+
+  process
+    .on('SIGINT', exitHandler)
+    .on('SIGTERM', exitHandler)
+    .on('SIGUSR1', exitHandler)
+    .on('SIGUSR2', exitHandler)
+    .on('uncaughtException', exitHandler)
+    .on('unhandledRejection', exitHandler);
+}
+
+const bootServer = () => {
+  enableGracefulShutdown();
+
+  http.createServer(requestHandler).listen(port, () => {
     console.log('♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦');
-    console.log(`♦♦♦♦♦♦  Server is ready for usage on port - ${ port } ♦♦♦♦♦♦`);
+    console.log(`♦♦♦♦♦♦  Server is ready for usage on port - ${port} ♦♦♦♦♦♦`);
     console.log('♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦');
   });
+};
+
+bootServer();
