6bbfc2ac0da942f1f9f93c093c6af48a4ac1b088
Константин Песчанов <constantine.peschanov@gmail.com>
2021-11-23 21:43:35 +0200

    change root dir structure. Concat all homeworks in 1 project

--- hometask-01/helpers/helper1.js
+++
@@ -1,5 +0,0 @@
-module.exports = (goods, query) => {
-  const data = Object.entries(query);
-
-  return goods.filter(item => item[data.shift()] === data.pop());
-};
--- src/libs/http-response.js
+++ src/libs/http-response.js
@@ -1,14 +1,14 @@
 module.exports = {
-  errorResponse(res, code, data) {
+  errorResponse: (res, code, data) => {
     res.setHeader('Content-Type', 'application/json');
     res.statusCode = code;
 
     return res.end(JSON.stringify(data));
   },
-  successResponse(res, code, data) {
+  successResponse: (res, code, data) => {
     res.setHeader('Content-Type', 'application/json');
     res.statusCode = code;
 
     return res.end(JSON.stringify(data));
-  }
-}
+  },
+};
